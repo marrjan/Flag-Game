4b375333bb7fd2743f2c34526ae999e180e66005
marrjan <marjankhormaeipour@gmail.com>
2023-09-13 18:20:25 +0430

    commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let timeSecond = 30;
+const timeH = document.querySelector('h1');
+
+displayTime(30);
+
+const countDown = setInterval(()=>{
+  timeSecond--;
+  displayTime(timeSecond);
+  if(timeSecond == 0 || timeSecond < 1){
+    endCount();
+    clearInterval(countDown);
+  }
+}, 1000);
+
+function displayTime(second){
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  timeH.innerHTML = `
+  ${(min < 10) ? '0' : ''}${min}:${(sec < 10) ? '0' : ''}${sec}
+  `; 
+
+}
+
+function endCount(){
+  timeH.innerHTML = 'Time out';
+}
